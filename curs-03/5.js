96f28f9aceab5a04765583e2530ccfc18ff17121
mihai-dulgheru <63505058+mihai-dulgheru@users.noreply.github.com>
2021-11-03 15:08:33 +0200

    first commit

---
+++ curs-03/5.js
@@ -0,0 +1,40 @@
+class ParentType {
+    constructor(a) {
+        this.a = a;
+    }
+    doParent() {
+        console.log('doing parent stuff with a ' + this.a);
+    }
+}
+
+class ChildType extends ParentType {
+    constructor(a, b) {
+        super(a);
+        this.b = b;
+    }
+    doChild() {
+        console.log('doing child stuff with a and b ' + this.a + ' ' + this.b);
+    }
+}
+
+let c0 = new ChildType(1, 2);
+c0.doChild();
+
+c0.doParent();
+
+// c0.doNewStuff = 
+ChildType.prototype.doNewStuff = function () {
+    console.log('doing new stuff');
+};
+c0.doNewStuff();
+
+console.log(c0.doParent.length);
+
+let f = c0.doParent;
+
+// f()
+
+f.call(c0);
+
+let f1 = f.bind(c0);
+f1();
